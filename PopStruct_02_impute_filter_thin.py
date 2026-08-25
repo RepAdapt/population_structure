cb8014feff6ecaf348beba4c3fa31ddeb62fcb6e
Brandon Lind <lind.brandon.m@gmail.com>
2026-08-25 11:32:08 -0400

    Rename output_path to outdir in script

--- PopStruct_02_impute_filter_thin.py
+++ PopStruct_02_impute_filter_thin.py
@@ -4,7 +4,7 @@ Input VCF assumed to be filtered for missing individuals and missing data.
 
 Usage
 -----
-In the following command, "/scratch:/scratch" is an upper directory relative to output_path
+In the following command, "/scratch:/scratch" is an upper directory relative to outdir
     module load apptainer
     
     sif=$HOME/pop_struct/population-structure.sif
@@ -12,7 +12,7 @@ In the following command, "/scratch:/scratch" is an upper directory relative to 
     jobfile=$(
         apptainer exec -B "$HOME/pop_struct,/scratch:/scratch" $sif \
             conda run -n pop_struct \
-            python PopStruct_02_impute_filter_thin.py vcf output_path is_discrete
+            python PopStruct_02_impute_filter_thin.py vcf outdir is_discrete
     )
     
     cd $(dirname $jobfile) && sbatch $jobfile
@@ -21,7 +21,7 @@ Paramters
 ---------
 vcf : Path
     path to uncompressed (.vcf) filtered vcf
-output_path : Path
+outdir : Path
     path to output directory
 is_discrete : 'not_discrete' | Path
     If populations are considered discrete, a path is specified to a file with the first column as the individual ID, and
@@ -34,8 +34,8 @@ the basename of the vcf file (eg basename.vcf) is used as a prefix for output fi
 """
 from pythonimports import *
 
-def main(vcf, output_path, is_discrete, threads=32):
-    output_path = op.abspath(outdir)  # in case it's a relative path
+def main(vcf, outdir, is_discrete, threads=32):
+    outdir = op.abspath(outdir)  # in case it's a relative path
 
     if not vcf.endswith('.vcf'):
         raise Exception('Input VCF must be uncompressed (and end with .vcf)')
@@ -51,8 +51,8 @@ def main(vcf, output_path, is_discrete, threads=32):
         hierfstat_text += f'''echo HIERFSTAT
 #conda activate hierfstat
 #Rscript $HOME/pop_struct/hierfstat.R {basename}_imputed_maf-filtered.txt {is_discrete}
-cd {output_path}
-apptainer exec -B "$HOME,{output_path}:{output_path}" $sif \
+cd {outdir}
+apptainer exec -B "$HOME,{outdir}:{outdir}" $sif \
     conda run -n hierfstat \
     Rscript $HOME/pop_struct/hierfstat.R {basename}_imputed_maf-filtered.txt {is_discrete}
 
@@ -73,13 +73,13 @@ date
 
 echo PLINK
 
-cd {output_path}
+cd {outdir}
 
 module load apptainer
 
 sif=$HOME/pop_struct/population-structure.sif
 
-apptainer exec -B "$HOME/pop_struct,{output_path}:{output_path}" $sif \
+apptainer exec -B "$HOME/pop_struct,{outdir}:{outdir}" $sif \
     conda run -n lea_bigsnpr \
     plink --vcf {vcf} --make-bed --out {basename} --keep-allele-order --allow-extra-chr --set-missing-var-ids @:# --double-id
 date
@@ -87,11 +87,11 @@ date
 
 echo IMPUTE
 
-#Rscript $HOME/pop_struct/LEA_smnf_impute.R {vcf} {output_path} {threads}
-apptainer exec -B "$HOME/pop_struct,{output_path}:{output_path}" $sif \
+#Rscript $HOME/pop_struct/LEA_smnf_impute.R {vcf} {outdir} {threads}
+apptainer exec -B "$HOME/pop_struct,{outdir}:{outdir}" $sif \
     env R_LIBS_USER="" R_LIBS="" \
     conda run --no-capture-output -n lea_bigsnpr \
-    Rscript $HOME/pop_struct/LEA_smnf_impute.R {vcf} {output_path} {threads}
+    Rscript $HOME/pop_struct/LEA_smnf_impute.R {vcf} {outdir} {threads}
 
 date
 
@@ -101,7 +101,7 @@ echo LOSTRUCT
 #conda activate lostruct
 #Rscript $HOME/pop_struct/lostruct.R {basename}_imputed_maf-filtered.txt
 
-apptainer exec -B "$HOME/pop_struct,{output_path}:{output_path}" $sif \
+apptainer exec -B "$HOME/pop_struct,{outdir}:{outdir}" $sif \
     env R_LIBS_USER="" R_LIBS="" \
     conda run -n lea_bigsnpr \
     Rscript $HOME/pop_struct/lostruct.R {basename}_imputed_maf-filtered.txt
@@ -112,7 +112,7 @@ date
 {hierfstat_text}
 
 '''
-    shfile = f'{output_path}/shfiles/{job}.sh'
+    shfile = f'{outdir}/shfiles/{job}.sh'
     with open(shfile, 'w') as o:
         o.write(text)
     # sbatch(shfile)
@@ -124,6 +124,6 @@ if __name__ == '__main__':
     latest_commit(html=False)
     session_info.show(html=False)
     
-    thisfile, vcf, output_path, is_discrete = sys.argv
-    main(vcf, output_path, is_discrete)
+    thisfile, vcf, outdir, is_discrete = sys.argv
+    main(vcf, outdir, is_discrete)
     pass
